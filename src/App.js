1714178b17a43375e3639f8b5ab92a20172981e5
Bello Osagie <osagienoah04@gmail.com>
2023-07-27 11:08:28 -0700

    introduction to 'JSX'
    Co-authored-by: Bello Osagie: <osagienoah04@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    React.createElement("div", { className: "App" },
+      React.createElement("header", { className: "App-header" },
+        React.createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
+        React.createElement("p", null,
+          "Edit ",
+          React.createElement("code", null, "src/App.js"),
+          " and save to reload."
+        ),
+        React.createElement("a", {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer"
+        },
+          "Learn React"
+        )
+      )
+    )
   );
 }
 
