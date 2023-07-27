58573203b0d6c53fee2f5d308dae45f26a13fd44
Bello Osagie <osagienoah04@gmail.com>
2023-07-27 13:24:27 -0700

    dynamic values and lists in 'JSX'
    Co-authored-by: Bello Osagie <osagienoah04@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-
 function App() {
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Another comment' },
+    { id: 3, text: 'more than just two' }
+  ]
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    React.createElement("div", { className: "App" },
-      React.createElement("header", { className: "App-header" },
-        React.createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
-        React.createElement("p", null,
-          "Edit ",
-          React.createElement("code", null, "src/App.js"),
-          " and save to reload."
-        ),
-        React.createElement("a", {
-          className: "App-link",
-          href: "https://reactjs.org",
-          target: "_blank",
-          rel: "noopener noreferrer"
-        },
-          "Learn React"
-        )
-      )
-    )
+    <div className='container'>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      {/* Any expressions in curly braces when in JSX or with the component return statement - to go from JSX to JS */}
+      <p>5 + 5 = {5 + 5}</p>
+      <p>Random Number: {Math.random() * 10}</p>
+      <h2>{title.toUpperCase()}</h2>
+
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {
+            comments.map((comment, index) => (
+              <li key={index}>{comment.text}</li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
   );
 }
 
