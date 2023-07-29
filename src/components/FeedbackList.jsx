c791874fe71a5089ec54f549dad785af5b20dcf5
Bello Osagie <osagienoah04@gmail.com>
2023-07-29 09:50:01 -0700

    update README.md file - 'License section
    Co-authored-by: Bello Osagie <osagienoah04@gmail.com>

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,17 @@
+const FeedbackList = ({ feedback }) => {
+  console.log(feedback) // check the console 
+
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+
+  return (
+    <div className='feedback-list'>
+      {feedback.map((feedbackItem) => {
+        return <div key={feedbackItem.id}>{feedbackItem.rating}</div>;
+      })}
+    </div>
+  );
+};
+
+export default FeedbackList;
