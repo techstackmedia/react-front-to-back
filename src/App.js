a4debfa794eeb9a280417f61d0a81042e280d976
Bello Osagie <osagienoah04@gmail.com>
2023-07-29 10:00:53 -0700

    update README.md file - 'License section
    Co-authored-by: Bello Osagie <osagienoah04@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import FeedbackItem from './components/FeedbackItem'
+import { useState } from 'react'
+import FeedbackList from './components/FeedbackList'
 import Header from './components/Header'
+import FeedbackData from './data/FeedbackData'
+import FeedbackItem from './components/FeedbackItem'
 
 const App = () => {
+  const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
     <>
       <Header />
       <div className='container'>
+        <FeedbackList feedback={feedback} />
         <FeedbackItem />
       </div>
     </>
