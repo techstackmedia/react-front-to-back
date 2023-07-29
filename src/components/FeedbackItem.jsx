c179a8f79444d285c4cf9091a5b5f129fe7f6cd8
Bello Osagie <osagienoah04@gmail.com>
2023-07-29 09:48:53 -0700

    update README.md file - 'License section
    Co-authored-by: Bello Osagie <osagienoah04@gmail.com>

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -6,14 +6,11 @@ const FeedbackItem = () => {
   const [increment, setIncrement] = useState(10);
 
   const handleClick = () => {
-    // rating = 7 or text = 'This text is updated' will not work because state in React is immutable.
-    // That is it can't be directly changed, it has to be reset
     setRating(7);
     setText('This text is updated');
   };
 
   const handleClickIncrement = () => {
-    // if you need access to the previous state for example in the case of incrementing values
     setIncrement((prevState) => {
       return prevState + 1;
     });
@@ -21,8 +18,6 @@ const FeedbackItem = () => {
   
   return (
     <div className='card'>
-      {/* <div className="num-display">10</div>
-      <div className="text-display">This is an example of a feedback Item</div> */}
       <div className='num-display'>{rating}</div>
       <div className='text-display'>{text}</div>
       <button onClick={handleClick}>Change Text and Rating</button>
