98839b5d639bc1373cac3cfd0b3436be0271cb65
Bello Osagie <osagienoah04@gmail.com>
2023-07-30 14:41:23 -0700

    move custom button to shared folder
    Co-authored-by: Bello Osagie <osagienoah04@gmail.com>
    delete Button from component folder

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Card from './shared/Card';
-import Button from './Button';
+import Button from './shared/Button';
 
 const FeedbackForm = () => {
   const [text, setText] = useState('');
