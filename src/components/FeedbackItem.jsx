c2fa6e80618ac10818e42349366aebab187ef573
Bello Osagie <osagienoah04@gmail.com>
2023-07-31 06:24:18 -0700

    uninstall react-icons
    Co-authored-by: Bello Osagie <osagienoah04@gmail.com>

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import Card from './shared/Card';
-import closeIcon from '../images/closeIcon.svg'
-import editIcon from '../images/editIcon.svg'
+import closeIcon from '../images/closeIcon.svg';
+import editIcon from '../images/editIcon.svg';
 
 const FeedbackItem = ({ item, handleDeleteFeedback, handleEditFeedback }) => {
   const handleDeleteButton = () => {
@@ -16,10 +16,20 @@ const FeedbackItem = ({ item, handleDeleteFeedback, handleEditFeedback }) => {
     <Card>
       <div className='num-display'>{item.rating}</div>
       <button onClick={handleDeleteButton} className='close'>
-        <img src={closeIcon} alt='close icon' width={16} />
+        <img
+          src={closeIcon}
+          alt='close icon'
+          /* width={16} */ width={13.328}
+          height={13.328}
+        />
       </button>
       <button onClick={handleEditButton} className='edit'>
-        <img src={editIcon} alt='edit icon' width={16} />
+        <img
+          src={editIcon}
+          alt='edit icon'
+          /* width={16} */ width={13.328}
+          height={13.328}
+        />
       </button>
       <div className='text-display'>{item.text}</div>
     </Card>
