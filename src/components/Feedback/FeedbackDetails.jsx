0e74e51487860804463cb411f055f2d05e4643e6
Bello Osagie <osagienoah04@gmail.com>
2023-08-06 14:59:26 -0700

    refactoring

--- src/components/Feedback/FeedbackDetails.jsx
+++ src/components/Feedback/FeedbackDetails.jsx
@@ -7,10 +7,10 @@ import { useContext } from 'react';
 import FeedbackContext from '../../context/FeedbackContext';
 
 const FeedbackDetails = () => {
-  const {isFalse, feedback} = useContext(FeedbackContext)
+  const { isFalse, feedback } = useContext(FeedbackContext);
   const { id } = useParams();
   const detail = feedback.find((item) => {
-    return item?._id === id;
+    return item._id === id;
   });
 
   const dateString = formatDateAndTime(detail?.date);
@@ -30,32 +30,38 @@ const FeedbackDetails = () => {
           alignItems: 'center',
         }}
       >
-        <Card reverse={isFalse} sx={{ width: '90%' }}>
-          <p>
-            <b>Text: </b>
-            {detail?.text}
-          </p>
-          <p>
-            <b>Rating:</b> {detail?.rating}
-          </p>
-          <p>
-            <b>ID:</b> {detail?._id}
-          </p>
-          <p>
-            <b>Date:</b>{' '}
-            {dateString === 'Invalid Date Invalid Date NaN NaN Invalid Date'
-              ? null
-              : dateString}
-          </p>
-          <Button
-            type='button'
-            version='secondary'
-            onClick={onClick}
-            sx={{ position: 'relative', top: 20 }}
-          >
-            Back to Home
-          </Button>
-        </Card>
+        {detail ? (
+          <Card reverse={isFalse} sx={{ width: '90%' }}>
+            <p>
+              <b>Text: </b>
+              {detail?.text}
+            </p>
+            <p>
+              <b>Rating:</b> {detail?.rating}
+            </p>
+            <p>
+              <b>ID:</b> {detail?._id}
+            </p>
+            <p>
+              <b>Date:</b>{' '}
+              {dateString === 'Invalid Date Invalid Date NaN NaN Invalid Date'
+                ? null
+                : dateString}
+            </p>
+            <Button
+              type='button'
+              version='secondary'
+              onClick={onClick}
+              sx={{ position: 'relative', top: 20 }}
+            >
+              Back to Home
+            </Button>
+          </Card>
+        ) : (
+          <Card reverse={isFalse} sx={{ width: '90%' }}>
+            <h3 className='title'>No Feedback Details</h3>
+          </Card>
+        )}
       </div>
       <Routes>
         <Route path='/show' element={<Footer />} />
