980952fe17dc264d79e815709696565b08d4790f
Bello Osagie <osagienoah04@gmail.com>
2023-08-25 17:34:33 -0700

    correct proptype definition

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -16,12 +16,12 @@ const FeedbackStats = ({ feedback }) => {
   
   export default FeedbackStats;
 
-  FeedbackStats.prototype = {
+  FeedbackStats.propTypes = {
     feedback: PropTypes.arrayOf(
       PropTypes.shape({
-        id: PropTypes.number,
+        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
         text: PropTypes.string,
-        rating: PropTypes.string,
+        rating: PropTypes.number,
       })
     ),
   };
