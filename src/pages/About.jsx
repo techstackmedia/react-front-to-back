7fc6714331482e2eddc54e769ca341bfe962f522
Bello Osagie <osagienoah04@gmail.com>
2023-08-27 14:22:29 -0700

    feat: navigation button to home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,40 @@
-import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Card from '../components/shared/Card';
 import Footer from '../components/Footer';
 import AboutIcon from '../components/AboutIcon';
 import { useContext } from 'react';
 import FeedbackContext from '../context/FeedbackContext';
+import Button from '../components/shared/Button';
+import { useNavigate } from 'react-router';
 
 const About = () => {
-  const {isFalse} = useContext(FeedbackContext)
+  const { isFalse } = useContext(FeedbackContext);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    return navigate('/');
+  };
+
   return (
     <>
       <Header />
       <div className='container'>
-        <div>
-          <Card reverse={isFalse}>
-            <div className='about'>
-              <h1>About This Project</h1>
-              <p>
-                This is a React app to leave feedback for a product or service
-              </p>
-              <p>Version 1.0.0</p>
-              <Link to='/' style={{color: isFalse ? '#fff' : undefined}}>Back to Home</Link>
-            </div>
-          </Card>
-        </div>
+        <Card reverse={isFalse}>
+          <div className='about'>
+            <h1>About This Project</h1>
+            <p>
+              This is a React app to leave feedback for a product or service
+            </p>
+            <p>Version 1.0.0</p>
+            <Button
+              version='secondary'
+              onClick={handleClick}
+              style={{ color: isFalse ? '#fff' : undefined }}
+            >
+              Back to Home
+            </Button>
+          </div>
+        </Card>
       </div>
       <Footer />
       <AboutIcon />
