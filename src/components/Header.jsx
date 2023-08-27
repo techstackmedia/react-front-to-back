6c6ff1ba9046776b9fb0bab77de7dc3846275e7a
Bello Osagie <osagienoah04@gmail.com>
2023-08-27 11:50:33 -0700

    feat: link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-const Header = ({text, bgColor, textColor}) => {
+const Header = ({ text, bgColor, textColor }) => {
   const headerStyles = {
     backgroundColor: bgColor,
     color: textColor,
-  }
-  
+  };
+
   return (
     <header style={headerStyles}>
-      <div className="container">
-        <h2>{text}</h2>
-      </div> 
+      <div className='container'>
+        <h2>
+          <a href='/'>{text}</a>
+        </h2>
+      </div>
     </header>
   );
 };
@@ -18,14 +20,13 @@ const Header = ({text, bgColor, textColor}) => {
 Header.defaultProps = {
   text: 'Feedback UI',
   bgColor: 'rgba(0, 0, 0, 0.4)',
-  textColor: '#ff6a95'
-}
+  textColor: '#ff6a95',
+};
 
 Header.propTypes = {
   text: PropTypes.string,
   bgColor: PropTypes.string,
-  textColor: PropTypes.string
-}
+  textColor: PropTypes.string,
+};
 
 export default Header;
-
