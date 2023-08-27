5f4261590d44d79f115f80f99d86d80492daf72d
Bello Osagie <osagienoah04@gmail.com>
2023-08-27 12:34:29 -0700

    feat: link to active current about page && git push origin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,37 @@
 import { NavLink, useLocation } from 'react-router-dom';
 import Card from './shared/Card';
-import FeedbackContext from '../context/FeedbackContext';
-import { useContext } from 'react';
 
 const Footer = () => {
-  const {isFalse} = useContext(FeedbackContext)
   const { pathname } = useLocation();
   const footer = ['home', 'about'];
-
-  const ft = footer.map((item) => (
-    <div key={item}>
-      <NavLink
-        style={{
-          textDecoration:
-            (pathname === '/' && item === 'home') || (pathname === '/about' && item === 'about') ? 'underline' : 'none',
-          color: isFalse ? '#fff': undefined
-        }}
-        to={`/${item}` === '/home' ? '/' : item}
-      >
-        {item.toUpperCase()[0]}
-        {item.slice(1)}
-      </NavLink>
-    </div>
-  ));
+  const ft = footer.map((item) => {
+    console.log(item);
+    return (
+      <div key={item}>
+        <NavLink
+          style={{
+            textDecoration:
+              (pathname === '/' && item === 'home') ||
+              (pathname === '/about' && item === 'about')
+                ? 'underline'
+                : 'none',
+          }}
+          to={item === 'home' ? '/' : `/${item}`}
+        >
+          {item.toUpperCase()[0]}
+          {item.slice(1)}
+        </NavLink>
+      </div>
+    );
+  });
 
   return (
     <div className='container'>
-      <Card reverse={isFalse}>
+      <Card>
         <div className='footer'>{ft}</div>
       </Card>
     </div>
   );
 };
 
-export default Footer
+export default Footer;
