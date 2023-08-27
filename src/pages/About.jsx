8a8f1e9298139832d18683537d4353349de775d0
Bello Osagie <osagienoah04@gmail.com>
2023-08-27 14:19:46 -0700

    feat: navigation button to home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,19 @@
-import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Card from '../components/shared/Card';
 import Footer from '../components/Footer';
 import AboutIcon from '../components/AboutIcon';
+import Button from '../components/shared/Button';
+import { useNavigate } from 'react-router';
 import useFeedback from '../hooks/useFeedback';
 
 const About = () => {
-  const {isFalse} = useFeedback()
+  const { isFalse } = useFeedback();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    return navigate('/');
+  };
+  
   return (
     <>
       <Header />
@@ -19,7 +26,13 @@ const About = () => {
                 This is a React app to leave feedback for a product or service
               </p>
               <p>Version 1.0.0</p>
-              <Link to='/' style={{color: isFalse ? '#fff' : undefined}}>Back to Home</Link>
+              <Button
+                version='secondary'
+                onClick={handleClick}
+                style={{ color: isFalse ? '#fff' : undefined }}
+              >
+                Back to Home
+              </Button>
             </div>
           </Card>
         </div>
