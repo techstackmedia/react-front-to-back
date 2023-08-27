578187672fdcb5aceba136460a07d5d47830c90d
Bello Osagie <osagienoah04@gmail.com>
2023-08-27 11:49:41 -0700

    feat: link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-const Header = ({text, bgColor, textColor}) => {
+const Header = ({ text, bgColor, textColor }) => {
   // const headerStyles = {
   //   backgroundColor:'blue',
   //   color: 'red'
@@ -9,14 +9,16 @@ const Header = ({text, bgColor, textColor}) => {
   const headerStyles = {
     backgroundColor: bgColor,
     color: textColor,
-  }
+  };
   return (
     // <header style={{backgroundColor: 'blue', color: '#f00'}}>
     // <header style={headerStyles}>
     <header style={headerStyles}>
-      <div className="container">
-        <h2>{text}</h2>
-      </div> 
+      <div className='container'>
+        <h2>
+          <a href='/'>{text}</a>
+        </h2>
+      </div>
     </header>
   );
 };
@@ -24,14 +26,13 @@ const Header = ({text, bgColor, textColor}) => {
 Header.defaultProps = {
   text: 'Feedback UI',
   bgColor: 'rgba(0, 0, 0, 0.4)',
-  textColor: '#ff6a95'
-}
+  textColor: '#ff6a95',
+};
 
 Header.propTypes = {
   text: PropTypes.string,
   bgColor: PropTypes.string,
-  textColor: PropTypes.string
-}
+  textColor: PropTypes.string,
+};
 
 export default Header;
-
