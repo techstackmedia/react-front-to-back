ebae1493347c2e52aba0bf038dc071802fb6e8f5
Bello Osagie <osagienoah04@gmail.com>
2023-08-27 10:12:43 -0700

    feat: show error message when possible

--- src/components/Feedback/FeedbackList.jsx
+++ src/components/Feedback/FeedbackList.jsx
@@ -1,64 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
+import FeedbackContext from '../../context/FeedbackContext';
 import Pulse from '../Pulse';
-import useFeedback from '../../hooks/useFeedback';
 
 const FeedbackList = () => {
-  const { feedback, isLoading } = useFeedback();
-  const [is24HrFormat, setIs24HrFormat] = useState(true);
-
-  // Load the user's preference from localStorage on component mount
-  useEffect(() => {
-    const storedFormat = localStorage.getItem('timeFormat');
-    if (storedFormat) {
-      setIs24HrFormat(storedFormat === '24-hour');
-    }
-  }, []);
-
-  // Function to toggle the time format and update localStorage
-  const handleTimeToggle = () => {
-    setIs24HrFormat((prevFormat) => {
-      const newFormat = !prevFormat;
-      localStorage.setItem('timeFormat', newFormat ? '24-hour' : '12-hour');
-      return newFormat;
-    });
-  };
+  const { feedback, isLoading, error } = useContext(FeedbackContext);
 
   if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
 
   return isLoading ? (
-    <Pulse />
+    <>
+      <Pulse />
+      {error && <p className='error'>{error}</p>}
+    </>
   ) : (
     <div className='feedback-list'>
-      {/* Display the time format toggle button */}
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button
-          onClick={handleTimeToggle}
-          style={{
-            backgroundColor: '#fff',
-            border: '1px solid #fff',
-            borderRadius: 4,
-            cursor: 'pointer',
-            padding: '5px 10px'
-          }}
-        >
-          {is24HrFormat
-            ? 'Switch to 12-hour format'
-            : 'Switch to 24-hour format'}
-        </button>
-      </div>
-
       {feedback.map((feedbackItem) => {
-        // Pass the is24HourFormat state to the FeedbackItem component
-        return (
-          <FeedbackItem
-            item={feedbackItem}
-            key={feedbackItem._id}
-            is24HrFormat={is24HrFormat}
-          />
-        );
+        return <FeedbackItem item={feedbackItem} key={feedbackItem._id} />;
       })}
     </div>
   );
