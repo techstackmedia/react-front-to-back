4272d73c029aba850c2866476e89d1707f575d1a
Bello Osagie <osagienoah04@gmail.com>
2023-08-26 18:33:59 -0700

    set loading state to false when fetching data

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -9,7 +9,7 @@ const FeedbackProvider = ({ children }) => {
 
   const [feedback, setFeedback] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(true);
   const [itemToDelete, setItemToDelete] = useState(null);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
@@ -32,7 +32,7 @@ const FeedbackProvider = ({ children }) => {
       setIsLoading(false);
     } catch (error) {
       console.error('Error fetching data:', error);
-      setIsLoading(false);
+      setIsLoading(true);
     }
   };
 
