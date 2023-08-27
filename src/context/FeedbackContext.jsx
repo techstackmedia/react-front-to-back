fdc7240bbd588985e59e12d517f30328775ebc64
Bello Osagie <osagienoah04@gmail.com>
2023-08-27 06:58:22 -0700

    feat: don't show modal by default

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -10,7 +10,7 @@ const FeedbackProvider = ({ children }) => {
 
   const [feedback, setFeedback] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [showDeleteModal, setShowDeleteModal] = useState(true);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [itemToDelete, setItemToDelete] = useState(null);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
