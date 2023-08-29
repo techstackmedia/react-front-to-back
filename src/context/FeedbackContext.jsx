bf0deff4fff056fd42ad82d73afddc70d000e55f
Bello Osagie <osagienoah04@gmail.com>
2023-08-29 09:25:13 -0700

    build: create custom alert modal

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from 'react';
+import Modal from '../components/Modal';
 
 const FeedbackContext = createContext();
 
@@ -118,31 +119,8 @@ const FeedbackProvider = ({ children }) => {
     }
   };
 
-  const alertConfirmationModal = showDeleteModal && (
-    <div className='custom-modal' onClick={closeModal}>
-      <div className='modal-content'>
-        <h2>Confirmation</h2>
-        <p>Are you sure you want to delete this item?</p>
-        <div className='modal-actions'>
-          <button
-            type='button'
-            onClick={handleDeleteConfirmed}
-            className='btn-confirm'
-          >
-            Confirm
-          </button>
-          <button
-            type='button'
-            onClick={handleDeleteCancelled}
-            className='btn-cancel'
-          >
-            Cancel
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-
+  const alertConfirmationModal = showDeleteModal ? <Modal /> : null;
+  
   return (
     <FeedbackContext.Provider
       value={{
