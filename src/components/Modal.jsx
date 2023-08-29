f8243c19fc2bc66bc9e1590d7362e63d12d9dc69
Bello Osagie <osagienoah04@gmail.com>
2023-08-29 08:43:06 -0700

    build: create custom alert modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,24 +1,44 @@
+import { AnimatePresence, motion } from 'framer-motion';
+
 const Modal = ({
+  closeModal,
   handleDeleteConfirmed,
   handleDeleteCancelled,
-  closeModal,
-}) => {
-  return (
-    <div onClick={closeModal} className='custom-modal'>
-      <div className='modal-content'>
+}) => (
+  <AnimatePresence>
+    <motion.div
+      className='custom-modal'
+      onClick={closeModal}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <motion.div
+        className='modal-content'
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 0.5, opacity: 0 }}
+      >
         <h2>Confirmation</h2>
         <p>Are you sure you want to delete this item?</p>
         <div className='modal-actions'>
-          <button onClick={handleDeleteConfirmed} className='btn-confirm'>
+          <button
+            type='button'
+            onClick={handleDeleteConfirmed}
+            className='btn-confirm'
+          >
             Confirm
           </button>
-          <button onClick={handleDeleteCancelled} className='btn-cancel'>
+          <button
+            type='button'
+            onClick={handleDeleteCancelled}
+            className='btn-cancel'
+          >
             Cancel
           </button>
         </div>
-      </div>
-    </div>
-  );
-};
-
+      </motion.div>
+    </motion.div>
+  </AnimatePresence>
+);
 export default Modal;
