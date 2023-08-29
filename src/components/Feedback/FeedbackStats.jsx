469a69bd99659bd3a13665012c7b3e26107c2e5c
Bello Osagie <osagienoah04@gmail.com>
2023-08-29 10:10:57 -0700

    build: create Timer component

--- src/components/Feedback/FeedbackStats.jsx
+++ src/components/Feedback/FeedbackStats.jsx
@@ -1,24 +1,18 @@
 import useFeedback from '../../hooks/useFeedback';
 
 const FeedbackStats = () => {
-  const { feedback, currentDate } = useFeedback();
+  const { feedback } = useFeedback();
   const average =
     feedback.length === 0
       ? 0
       : feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length;
   const formattedAverage = average.toFixed(1).replace(/[.,]0$/, '');
-  const date = currentDate.split('⏲️')[0];
-  const time = `⏲️ ${currentDate.split('⏲️')[1]}`;
 
   return (
     <>
       <div className='feedback-stats'>
         <h4>{feedback.length} Reviews</h4>
         <h4>Average Rating: {formattedAverage}</h4>
-      </div>
-      <div className='feedback-stats'>
-        <h5>{date}</h5>
-        <h5>{time}</h5>
       </div>
     </>
   );
