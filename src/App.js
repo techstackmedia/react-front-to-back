b4119c11f2570f88c4a6e9bc704eae6505837509
Bello Osagie <osagienoah04@gmail.com>
2023-08-29 06:46:11 -0700

    custom alert modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
+import Modal from './components/Modal';
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -24,20 +25,10 @@ const App = () => {
   };
 
   const alertConfirmationModal = showDeleteModal && (
-    <div className='custom-modal'>
-      <div className='modal-content'>
-        <h2>Confirmation</h2>
-        <p>Are you sure you want to delete this item?</p>
-        <div className='modal-actions'>
-          <button onClick={handleDeleteConfirmed} className='btn-confirm'>
-            Confirm
-          </button>
-          <button onClick={handleDeleteCancelled} className='btn-cancel'>
-            Cancel
-          </button>
-        </div>
-      </div>
-    </div>
+    <Modal
+      handleDeleteConfirmed={handleDeleteConfirmed}
+      handleDeleteCancelled={handleDeleteCancelled}
+    />
   );
 
   return (
