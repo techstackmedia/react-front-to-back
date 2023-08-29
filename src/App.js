206df50d49cada39be69387c3f1fb7dcc3362eaa
Bello Osagie <osagienoah04@gmail.com>
2023-08-29 07:02:28 -0700

    build: create custom alert modal

--- src/App.js
+++ src/App.js
@@ -40,29 +40,6 @@ const App = () => {
     />
   ) : null;
 
-  <div className='custom-modal' onClick={closeModal}>
-    <div className='modal-content'>
-      <h2>Confirmation</h2>
-      <p>Are you sure you want to delete this item?</p>
-      <div className='modal-actions'>
-        <button
-          type='button'
-          onClick={handleDeleteConfirmed}
-          className='btn-confirm'
-        >
-          Confirm
-        </button>
-        <button
-          type='button'
-          onClick={handleDeleteCancelled}
-          className='btn-cancel'
-        >
-          Cancel
-        </button>
-      </div>
-    </div>
-  </div>;
-
   return (
     <>
       <Header />
