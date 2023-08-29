dfeaf3b71dc0c5d1c922ca03fe7347ec989e40cc
Bello Osagie <osagienoah04@gmail.com>
2023-08-29 06:03:56 -0700

    build: create timer component

--- src/components/Feedback/FeedbackStats.jsx
+++ src/components/Feedback/FeedbackStats.jsx
@@ -1,16 +1,14 @@
 import { useContext } from 'react';
 import FeedbackContext from '../../context/FeedbackContext';
+import Timer from '../Timer';
 
 const FeedbackStats = () => {
-  const { feedback, currentDate } = useContext(FeedbackContext);
+  const { feedback } = useContext(FeedbackContext);
   const average =
     feedback.length === 0
       ? 0
       : feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length;
   const formattedAverage = average.toFixed(1).replace(/[.,]0$/, '');
-  const date = currentDate.split('⏲️')[0];
-  // const time = `${currentDate.split('⏲️')[1]}`;
-  const time = `⏲️ ${currentDate.split('⏲️')[1]}`;
 
   return (
     <>
@@ -18,11 +16,7 @@ const FeedbackStats = () => {
         <h4>{feedback.length} Reviews</h4>
         <h4>Average Rating: {formattedAverage}</h4>
       </div>
-      <div className='feedback-stats' /* style={{position: 'relative'}} */>
-        <h5>{date}</h5>
-        {/* <div style={{position: 'absolute', right: 75}}>⏲️</div> */}
-        <h5>{time}</h5>
-      </div>
+      <Timer />
     </>
   );
 };
