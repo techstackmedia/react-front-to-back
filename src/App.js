172328f4bef4ed4ffd561fdfd4eec4a992f380be
Bello Osagie <osagienoah04@gmail.com>
2023-08-29 08:30:51 -0700

    build: create custom alert modal

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import { v4 } from 'uuid';
-import { AnimatePresence, motion } from 'framer-motion';
+import Modal from './components/Modal';
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -40,41 +40,11 @@ const App = () => {
   };
 
   const alertConfirmationModal = showDeleteModal && (
-    <AnimatePresence>
-      <motion.div
-        className='custom-modal'
-        onClick={closeModal}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        <motion.div
-          className='modal-content'
-          initial={{ scale: 0.5, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
-          exit={{ scale: 0.5, opacity: 0 }}
-        >
-          <h2>Confirmation</h2>
-          <p>Are you sure you want to delete this item?</p>
-          <div className='modal-actions'>
-            <button
-              type='button'
-              onClick={handleDeleteConfirmed}
-              className='btn-confirm'
-            >
-              Confirm
-            </button>
-            <button
-              type='button'
-              onClick={handleDeleteCancelled}
-              className='btn-cancel'
-            >
-              Cancel
-            </button>
-          </div>
-        </motion.div>
-      </motion.div>
-    </AnimatePresence>
+    <Modal
+      closeModal={closeModal}
+      handleDeleteCancelled={handleDeleteCancelled}
+      handleDeleteConfirmed={handleDeleteConfirmed}
+    />
   );
 
   return (
