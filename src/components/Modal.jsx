66d0c2c18ba6777d19f4a945767ff1dd2f587018
Bello Osagie <osagienoah04@gmail.com>
2023-08-29 08:57:40 -0700

    build: create custom alert modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
+
+const Modal = () => {
+  const { closeModal, handleDeleteConfirmed, handleDeleteCancelled } =
+    useContext(FeedbackContext);
+  return (
+    <div
+      className='custom-modal'
+      onClick={closeModal}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div
+        className='modal-content'
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 0.5, opacity: 0 }}
+      >
+        <h2>Confirmation</h2>
+        <p>Are you sure you want to delete this item?</p>
+        <div className='modal-actions'>
+          <button
+            type='button'
+            onClick={handleDeleteConfirmed}
+            className='btn-confirm'
+          >
+            Confirm
+          </button>
+          <button
+            type='button'
+            onClick={handleDeleteCancelled}
+            className='btn-cancel'
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
